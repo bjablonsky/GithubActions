d728b6260898bd5c716f5893a115585e6fe826f2
Brian Jablonsky <bjablonsky@users.noreply.github.com>
2020-06-18 13:56:12 -0400

    Initial commit of code

---
+++ src/GithubActionsExample.Web/Pages/API.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace GithubActionsTest.Pages
+{
+    public class APIModel : PageModel
+    {
+        private readonly ILogger<APIModel> _logger;
+
+        public APIModel(ILogger<APIModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
